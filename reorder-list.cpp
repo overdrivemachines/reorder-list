4f4a036e0d1ee2e4b1c9b35c25746766f229387d
get.dipen@gmail.com <get.dipen@gmail.com>
2023-03-07 23:52:01 -0800

    added desc

--- reorder-list.cpp
+++ reorder-list.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// Create Linked list given a vector of int values
+// returns pointer to head of lined list
+ListNode* createLinkedList(vector<int> values) {
+  ListNode* head;
+  ListNode* node = new ListNode();
+  head = node;
+  for (int value : values) {
+    node->next = new ListNode(value);
+    node = node->next;
+  }
+  head = head->next;
+  return head;
+}
+
+// Prints Linked List
+// e.g. 5->8->3->NULL
+void printLinkedList(ListNode* head) {
+  ListNode* node = head;
+
+  while (node != nullptr) {
+    cout << node->val << "->";
+    node = node->next;
+  }
+  cout << "NULL\n";
+}
+
+void reorderList(ListNode* head) {
+}
+
+int main(int argc, char const* argv[]) {
+  ListNode* n = createLinkedList({1, 2, 3, 4});
+  printLinkedList(n);
   return 0;
 }
