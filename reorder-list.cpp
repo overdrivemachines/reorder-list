f87cd5cad3d74e04e6ea9ccc7253931f095c36de
get.dipen@gmail.com <get.dipen@gmail.com>
2023-03-09 00:38:28 -0800

    found solution

--- reorder-list.cpp
+++ reorder-list.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 
 using namespace std;
@@ -37,11 +38,79 @@ void printLinkedList(ListNode* head) {
   cout << "NULL\n";
 }
 
+// Version 1:
+//    Runtime 50 ms Beats 32.20%
+//    Memory 18.6 MB Beats 16.61%
+void reorderList1(ListNode* head) {
+  // do nothing for link lists that have 0, 1 or 2 elements
+  if ((head == nullptr) || (head->next == nullptr) || (head->next->next == nullptr))
+    return;
+
+  int num_nodes = 0, num = 0;
+  ListNode* node = head;
+  ListNode* temp;
+  stack<ListNode*> stack_nodes;
+
+  // add all the nodes on to the stack
+  while (node != nullptr) {
+    stack_nodes.push(node);
+    node = node->next;
+  }
+
+  num_nodes = stack_nodes.size();
+  node = head;
+  while (num < num_nodes / 2) {
+    temp = node->next;  // 3
+    // set the next node
+    node->next = stack_nodes.top();  // 4
+    stack_nodes.pop();
+
+    node = node->next;  // 4
+    node->next = temp;  // 3
+    node = node->next;  // 3
+    num++;
+  }
+
+  node->next = nullptr;
+}
+
 void reorderList(ListNode* head) {
+  // do nothing for link lists that have 0, 1 or 2 elements
+  if ((head == nullptr) || (head->next == nullptr) || (head->next->next == nullptr))
+    return;
+
+  int num_nodes = 0, num = 0;
+  ListNode* node = head;
+  ListNode* temp;
+  stack<ListNode*> stack_nodes;
+
+  // add all the nodes on to the stack
+  while (node != nullptr) {
+    stack_nodes.push(node);
+    node = node->next;
+  }
+
+  num_nodes = stack_nodes.size();
+  node = head;
+  while (num < num_nodes / 2) {
+    temp = node->next;  // 3
+    // set the next node
+    node->next = stack_nodes.top();  // 4
+    stack_nodes.pop();
+
+    node = node->next;  // 4
+    node->next = temp;  // 3
+    node = node->next;  // 3
+    num++;
+  }
+
+  node->next = nullptr;
 }
 
 int main(int argc, char const* argv[]) {
   ListNode* n = createLinkedList({1, 2, 3, 4});
+  printLinkedList(n);
+  reorderList(n);
   printLinkedList(n);
   return 0;
 }
